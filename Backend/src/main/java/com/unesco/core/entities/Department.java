f22b484bdcb47342fef1deb4a4da73b1a0a70784
RuiFoxx <fox.uchiha95@gmail.com>
2018-03-13 12:25:22 +0700

    [!!!] NEVER DO THIS SHIT AGAIN! COMMITED AND FORGOTTEN!

--- Backend/src/main/java/com/unesco/core/entities/Department.java
+++ Backend/src/main/java/com/unesco/core/entities/Department.java
@@ -47,17 +47,17 @@ public class Department {
         this.institute = institute;
     }
 
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "department")
-    /** Поле набор планов */
-    private Set<Group> groups;
-
-    public Set<Group> getGroups() {
-        return groups;
-    }
-
-    public void setGroups(Set<Group> groups) {
-        this.groups = groups;
-    }
+//    @OneToMany(cascade = CascadeType.ALL, mappedBy = "department")
+//    /** Поле набор планов */
+//    private Set<Group> groups;
+//
+//    public Set<Group> getGroups() {
+//        return groups;
+//    }
+//
+//    public void setGroups(Set<Group> groups) {
+//        this.groups = groups;
+//    }
 
     public Department(String name) {
         this.name = name;
