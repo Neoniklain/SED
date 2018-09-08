39d982c299037fdc10fc120f05df2ccc859e764b
neoniklain <vet-vash@yandex,ru>
2018-09-08 22:16:17 +0700

    Кэширование в расписании.

--- Frontend/src/app/components/schedule/showSchedule/show-schedule.component.ts
+++ Frontend/src/app/components/schedule/showSchedule/show-schedule.component.ts
@@ -4,7 +4,6 @@ import {RouteConstants} from "../../../bootstrap/app.route.constants";
 import {Group} from "../../../models/shedule/group";
 import {Router} from "@angular/router";
 import {DictionaryService} from "../../../services/dictionary.service";
-import {AccountService} from "../../../services/accountService";
 import {ScheduleService} from "../../../services/schedule.service";
 import {Pair} from "../../../models/shedule/pair";
 import {Department} from "../../../models/shedule/department";
@@ -12,24 +11,30 @@ import {DepartmentShedule} from "../../../models/shedule/departmentShedule";
 import {NotificationService} from "../../../services/notification.service";
 import {Dictionary} from "../../../models/admin/dictionary.model";
 import {isUndefined} from "util";
+import {LazyLoadEvent} from "primeng/api";
 
 @Component({
-    selector: 'schedule-page',
-    templateUrl: './show-schedule.component.html',
-    styleUrls: ["./show-schedule.component.css"]
-})
+        selector: 'schedule-page',
+        templateUrl: './show-schedule.component.html',
+        styleUrls: ["./show-schedule.component.css"]
+    })
 
-export class ShowScheduleComponent implements OnInit {
+    export class ShowScheduleComponent implements OnInit {
 
     @Input() editable: boolean = false;
-    public profs: Array<Professor> = new Array();
-    public groups: Array<Group> = new Array();
-    public departments: Array<Department> = new Array();
     public RouteConstants = RouteConstants;
     public menuToggle: string = "professor";
     public pairList: Array<Pair> = null;
     public templatePair: Pair;
     public departmentSchedule: Array<DepartmentShedule>;
+
+    public profs: Array<Professor> = [];
+    public groups: Array<Group> = [];
+    public departments: Array<Department> = [];
+
+    public lastprofsFilter: LazyLoadEvent = { globalFilter: "&7&" };
+    public lastgroupsFilter: LazyLoadEvent = { globalFilter: "&7&" };
+    public lastdepartmentsFilter: LazyLoadEvent = { globalFilter: "&7&" };
 
     public currentProfessor: Professor;
     public currentGroup: Group;
@@ -41,8 +46,7 @@ export class ShowScheduleComponent implements OnInit {
                 private router: Router,
                 private ScheduleService: ScheduleService,
                 private dictionaryService: DictionaryService,
-                private accountService: AccountService,
-                ) {
+    ) {
     }
 
     ngOnInit() {
@@ -60,61 +64,51 @@ export class ShowScheduleComponent implements OnInit {
     }
 
     public GetProfessors(event: any) {
-        let filter;
-        if (!isUndefined(event.query)) {
-            filter = {
-                globalFilter: event.query.substring(0, 60)
-            };
+        if (this.profs.length == 0 || this.lastprofsFilter.globalFilter != event.query.substring(0, 60)) {
+            if (!isUndefined(event.query))
+                this.lastprofsFilter = { globalFilter: event.query.substring(0, 60) };
+
+            this.dictionaryService.Get(Dictionary.professors, this.lastprofsFilter).subscribe(
+                result => {
+                    if (result.content.length > 0)
+                        this.profs = result.content;
+                }
+            );
+        } else {
+            this.profs = JSON.parse(JSON.stringify(this.profs));
         }
-        let temp: Array<Professor> = new Array();
-        this.dictionaryService.Get(Dictionary.professors, filter).subscribe(
-            result => {
-                console.log("result", result);
-                temp = result.content;
-                if (temp.length > 0) {
-                    this.profs = temp;
-                }
-                else {
-                    this.profs = [];
-                }
-            }, error => console.error(error)
-        );
     }
+
     public GetGroups(event: any) {
-        let filter;
-        if (!isUndefined(event.query)) {
-            filter = {
-                globalFilter: event.query.substring(0, 60)
-            };
+        if (this.groups.length == 0 || this.lastgroupsFilter.globalFilter != event.query.substring(0, 60)) {
+            if (!isUndefined(event.query))
+                this.lastgroupsFilter = { globalFilter: event.query.substring(0, 60) };
+
+            this.dictionaryService.Get(Dictionary.groups, this.lastgroupsFilter).subscribe(
+                result => {
+                    if (result.content.length > 0)
+                        this.groups = result.content;
+                }
+            );
+        } else {
+            this.groups = JSON.parse(JSON.stringify(this.groups));
         }
-        let temp: Array<Group> = new Array();
-        this.dictionaryService.Get(Dictionary.groups, filter).subscribe(
-            result => {
-                temp = result.content;
-                if (temp.length > 0)
-                    this.groups = temp;
-                else
-                    this.groups = [];
-            }, error => console.error(error)
-        );
     }
+
     public GetDepartments(event: any) {
-        let filter;
-        if (!isUndefined(event.query)) {
-            filter = {
-                globalFilter: event.query.substring(0, 60)
-            };
+        if (this.departments.length == 0 || this.lastdepartmentsFilter.globalFilter != event.query.substring(0, 60)) {
+            if (!isUndefined(event.query))
+                this.lastdepartmentsFilter = { globalFilter: event.query.substring(0, 60) };
+
+            this.dictionaryService.Get(Dictionary.departments, this.lastdepartmentsFilter).subscribe(
+                result => {
+                    if (result.content.length > 0)
+                        this.departments = result.content;
+                }
+            );
+        } else {
+            this.departments = JSON.parse(JSON.stringify(this.departments));
         }
-        let temp: Array<Department> = new Array();
-        this.dictionaryService.Get(Dictionary.departments, filter).subscribe(
-            result => {
-                temp = result.content;
-                if (temp.length > 0)
-                    this.departments = temp;
-                else
-                    this.departments = [];
-            }, error => console.error(error)
-        );
     }
 
     public getProfessorPair() {
