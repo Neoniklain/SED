778a74613da53f8188908ad8dc1efb0bb90c6347
Valentin Karpov <dremwerk@rambler.ru>
2018-09-15 18:07:17 +0700

    Удалил старую реализацию (сервер)

--- Backend/src/main/java/com/unesco/core/services/dataService/taskService/taskDescriptionService/ITaskDescriptionDataService.java
+++ Backend/src/main/java/com/unesco/core/services/dataService/taskService/taskDescriptionService/ITaskDescriptionDataService.java
@@ -6,16 +6,4 @@ import com.unesco.core.services.dataService.IDataService;
 import java.util.List;
 
 public interface ITaskDescriptionDataService extends IDataService<TaskDescriptionModel> {
-    /**
-     * Возвращает список описаний задач для автора
-     * @param id id автора
-     * @return
-     */
-    List<TaskDescriptionModel> getTaskDescriptionByCreator(long id);
-
-    /**
-     * Обновление описания задачи
-     * @param td новые данные описания
-     */
-    void updateTaskDescription(TaskDescriptionModel td);
 }
--- Backend/src/main/java/com/unesco/core/services/dataService/taskService/taskUserService/ITaskUserDataService.java
+++ Backend/src/main/java/com/unesco/core/services/dataService/taskService/taskUserService/ITaskUserDataService.java
@@ -6,22 +6,4 @@ import com.unesco.core.services.dataService.IDataService;
 import java.util.List;
 
 public interface ITaskUserDataService extends IDataService<TaskUserModel> {
-    /**
-     * Возвращает список задач для указанного описания задачи
-     * @param id id описания задачи
-     * @return
-     */
-    List<TaskUserModel> getTaskUserByTaskDescription(long id);
-
-    /**
-     * Обновление конкретной реализации задачи
-     * @param tu новые данные реализации
-     */
-    void updateTaskUser(TaskUserModel tu);
-
-    /**
-     * Возвращает список реализаций задач, в которых пользователь является исполнителем.
-     * @param id id пользователя
-     */
-    List<TaskUserModel> getTaskUsersByExecutor(long id);
 }
