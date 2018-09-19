38796ec65f7d8ad24be749614cdcd8ab3b1007b5
neoniklain <vet-vash@yandex,ru>
2018-09-19 21:53:24 +0700

    Изменил url продакшен сервера.

--- Frontend/src/app/bootstrap/app.environment.ts
+++ Frontend/src/app/bootstrap/app.environment.ts
@@ -12,12 +12,7 @@ export let appSettings = {
     BaseApiUrl: '',
 };
 
-let environment = Enviromnent.Development;
-// / #if ENVIRONMENT == "prod"
-environment = Enviromnent.Production;
-appSettings.BaseApiUrl = 'http://62.173.145.143:8080/api/';
-/// #endif
-/// #if ENVIRONMENT == "dev"
-environment = Enviromnent.Development;
+// Prodaction
+// appSettings.BaseApiUrl = 'http://192.168.253.8:8080/api/';
+// local
 appSettings.BaseApiUrl = 'http://localhost:8080/api/';
-/// #endif
