4640df2a5d7e8b0fd818d0b5faff962f73471ccd
Valentin Karpov <dremwerk@rambler.ru>
2019-03-16 22:18:40 +0700

    Добавил 2 варианта подключения к moodle. Не знаю как это будет работать локально. Скорее всего через IP. Удалённо проверил - работает.

--- Backend/Core/src/main/java/com/unesco/core/config/MoodleConfig/MoodleConfigService.java
+++ Backend/Core/src/main/java/com/unesco/core/config/MoodleConfig/MoodleConfigService.java
@@ -8,17 +8,20 @@ public class MoodleConfigService implements IMoodleConfigService {
 
     public String GetURL(){
         // Удалённое подключение (Prod)
-        //return "http://localhost/moodle/webservice/rest/server.php";
+        // Вариант №1
+        return "http://192.168.253.9/moodle/webservice/rest/server.php";
+        // Вариант №2
+        // return "http://ictkem.ru:84/moodle/webservice/rest/server.php";
 
         // Локальное подключение (Dev)
-        return "http://localhost/moodle/webservice/rest/server.php";
+        //return "http://localhost/moodle/webservice/rest/server.php";
     }
 
     public String GetToken() {
         // Токен удалённого подключения (Prod)
-        //return "0696880e69359bb66a713931a44c9974";
+        return "b6d863d7db8c12e55df671ffc7b23f1b";
 
         // Токен локального подключения (Dev)
-        return "0696880e69359bb66a713931a44c9974";
+        //return "0696880e69359bb66a713931a44c9974";
     }
 }
